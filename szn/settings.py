12cf333b0f95ed9aaeeae0aac0e46ae3c81add18
zazajo <Josephedward201@gmail.com>
2026-01-20 16:33:59 +0100

    Update settings.py

--- szn/settings.py
+++ szn/settings.py
@@ -173,8 +173,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Keep other variables for reference
-EMAIL_BACKEND = 'resend.django.EmailBackend'
-RESEND_API_KEY = os.environ.get('RESEND_API_KEY') 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.resend.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'resend'
+EMAIL_HOST_PASSWORD = os.environ.get('RESEND_API_KEY') 
 DEFAULT_FROM_EMAIL = 'noreply@sznisreal.com'
 RECIPIENT_EMAIL = 'markirving012@gmail.com'
 
