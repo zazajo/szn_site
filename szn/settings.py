35deab9a8c08930df9d562ae634abad8039a5e5f
zazajo <Josephedward201@gmail.com>
2026-01-21 15:16:37 +0100

    Update settings.py

--- szn/settings.py
+++ szn/settings.py
@@ -177,9 +177,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Keep other variables for reference
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.resend.com'
-EMAIL_PORT = 465
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True
+EMAIL_PORT = 2525
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 EMAIL_HOST_USER = 'resend'
 EMAIL_HOST_PASSWORD = os.environ.get('RESEND_API_KEY') 
 DEFAULT_FROM_EMAIL = 'onboarding@resend.dev'
